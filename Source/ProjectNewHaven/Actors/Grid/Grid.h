138c393a278afb4e52bfd80750a89a7af502a752
cacayperez <cacayperez@outlook.com>
2022-02-23 20:40:32 +0800

    added grid base actor class

---
+++ Source/ProjectNewHaven/Actors/Grid/Grid.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Grid.generated.h"
+
+UCLASS()
+class PROJECTNEWHAVEN_API AGrid : public AActor
+{
+	GENERATED_BODY()
+
+	
+public:
+	// Sets default values for this actor's properties
+	AGrid();
+
+};
